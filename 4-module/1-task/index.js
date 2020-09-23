eccd1ba71172b7f93af380329379c1e7fbcd7ea3
gabrusenas.ar <gabrusenas@outlook.com>
2020-09-23 19:37:09 +0300

    добавил решение 1-2 задачи 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,6 +3,20 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+
+  let list = friends.map(function (item) {
+    let li = document.createElement("li");
+    li.textContent = item.firstName + " " + item.lastName;
+
+    return li;
+  });
+
+  list.forEach(function (item) {
+    ul.appendChild(item);
+  });
+
+  return ul;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,7 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let tr of table.rows) {
+    tr.cells[tr.rowIndex].style.backgroundColor = "red";
+  }
 }
