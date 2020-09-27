00d6faefca46d209da9c220acdb0474e07a1c165
gabrusenas.ar <gabrusenas@outlook.com>
2020-09-27 20:47:57 +0300

    добавил решение решение 1-3 задачи 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+  let hiddenText = false;
+  button.addEventListener("click", function () {
+    text.hidden = hiddenText = !hiddenText;
+  });
 }
